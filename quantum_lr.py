535ee7c143f4137bb3fd91006c66fd723dc7821b
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-08-08 08:20:26 +0000

    working not tested too much

---
+++ quantum_lr.py
@@ -0,0 +1,79 @@
+import networkx as nx
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+from dwave.system import DWaveSampler, EmbeddingComposite
+
+N = 10
+d = 4
+precision = 3
+dim = (d + 1) * precision
+
+data = np.random.rand(N, d)
+# Y = np.random.rand(N)
+Y = data[:, 0] + 1.5 * data[:, 2] + data[:, 3] * 0.25
+X = np.ones((N, d + 1))
+X[:, :-1] = data
+XtX = np.matmul(X.T, X)
+XtY = np.matmul(X.T, Y)
+
+# Creating the graph
+G = nx.Graph()
+G.add_edges_from([(i, j) for i in range(dim) for j in range(i + 1, dim)])
+
+# The matrix where we add the objective and the constraint
+Q = defaultdict(int)
+
+# Now consider the objective function. It is one big function divided into different blocks
+# For now, the weights are [1, 0.5, 0.25 ...] depending on the precision bits required
+
+# First term, same weights
+for i in range(d + 1):
+    xii = XtX[i, i]
+    for k in range(precision):
+        d1 = i * precision + k
+        Q[(d1, d1)] += xii / pow(2, 2 * k)
+        for l in range(k + 1, precision):
+            d2 = i * precision + l
+            Q[(d1, d2)] += 2 * xii / pow(2, k + l)
+
+# First term, different weights
+for i in range(d + 1):
+    for j in range(i + 1, d + 1):
+        xij = XtX[i, j]
+        for k in range(precision):
+            for l in range(precision):
+                d1 = i * precision + k
+                d2 = j * precision + l
+                Q[(d1, d2)] += 2 * xij / pow(2, k + l)
+
+
+# Second Term
+for i in range(d + 1):
+    xyi = XtY[i]
+    for k in range(precision):
+        d1 = i * precision + k
+        Q[(d1, d1)] -= 2 * xyi / pow(2, k)
+
+
+sampler = EmbeddingComposite(DWaveSampler())
+sampleset = sampler.sample_qubo(Q, num_reads=20, chain_strength=1)
+
+# Print the entire sampleset, that is, the entire table
+print(sampleset)
+
+distributions = []
+
+for sample, energy in sampleset.data(['sample', 'energy']):
+    distributions.append(sample)
+
+sol_no = 1
+for di in distributions:
+    wts = [0.0 for i in range(d + 1)]
+    for x in range(dim):
+        i = x // precision
+        k = x % precision # The p^th of the bits we are using to represent the i^th item
+        wts[i] += di[x] / pow(2, k)
+    print(str(sol_no) + "-")
+    print("Weights:", wts)
+    sol_no += 1
