19831da4b943abe05f323f09d9389bc9ae8624fa
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-08-16 04:18:01 +0000

    code done

--- quantum_lr_fs_precision_list.py
+++ quantum_lr_fs_precision_list.py
@@ -19,17 +19,33 @@ def select_features(X_train, y_train, features):
 np.random.seed(42)
 
 N = 10000
-d = 20
+d = 6
 factor = 2
-precision = 2
-dim = (d + 1) * precision
 
 data = np.random.rand(N, d*factor)
 Y = np.random.rand(N)
 
 data_fs, fs = select_features(data,Y,d)
 importance = fs.scores_
-# Y = 0.5 * data[:, 0] + 1.25 * data[:, 1] + 0.5 * data[:,2]
+
+D = factor*d
+high = sum(importance)
+importance /= high
+precision_list = list(map(math.ceil,importance*64))
+
+assert(d == len(precision_list))
+for i in range(d):
+    assert(precision_list[i] != 0)
+
+precision_list.append(1) # Constant term at the end, has 1 precision only
+dim = sum(precision_list)
+
+# Prefix sum of precision
+pref_prec = [0]
+for i in range(len(precision_list)):
+    pref_prec.append(pref_prec[-1] + precision_list[i])
+
+
 X = np.ones((N, d + 1))
 X[:, :-1] = data_fs
 X_sk = np.ones((N, factor*d + 1))
@@ -45,41 +61,39 @@ G.add_edges_from([(i, j) for i in range(dim) for j in range(i + 1, dim)])
 Q = defaultdict(int)
 
 # Now consider the objective function. It is one big function divided into different blocks
-# For now, the weights are [1, 0.5, 0.25 ...] depending on the precision bits required
+# he weights are [0.25, 0.5, 1 ...] depending on the precision bits offered
 
 # First term, same weights
-D = factor*d
-high = sum(importance)
-importance /= high
-precision_list = list(map(math.ceil,importance*64))
-
-
 for i in range(d + 1):
     xii = XtX[i, i]
+    precision = pref_prec[i + 1] - pref_prec[i]
     for k in range(precision):
-        d1 = i * precision + k
-        Q[(d1, d1)] += xii / pow(2, 2 * k)
+        d1 = pref_prec[i] + k
+        Q[(d1, d1)] += xii / pow(2, 2 * (k - 2))
         for l in range(k + 1, precision):
-            d2 = i * precision + l
-            Q[(d1, d2)] += 2 * xii / pow(2, k + l)
+            d2 = pref_prec[i] + l
+            Q[(d1, d2)] += 2 * xii / pow(2, (k - 2) + (l - 2))
 
 # First term, different weights
 for i in range(d + 1):
     for j in range(i + 1, d + 1):
         xij = XtX[i, j]
-        for k in range(precision):
-            for l in range(precision):
-                d1 = i * precision + k
-                d2 = j * precision + l
-                Q[(d1, d2)] += 2 * xij / pow(2, k + l)
+        precision1 = pref_prec[i + 1] - pref_prec[i]
+        precision2 = pref_prec[j + 1] - pref_prec[j]
+        for k in range(precision1):
+            for l in range(precision2):
+                d1 = pref_prec[i] + k
+                d2 = pref_prec[j] + l
+                Q[(d1, d2)] += 2 * xij / pow(2, (k - 2) + (l - 2))
 
 
 # Second Term
 for i in range(d + 1):
     xyi = XtY[i]
+    precision = pref_prec[i + 1] - pref_prec[i]
     for k in range(precision):
-        d1 = i * precision + k
-        Q[(d1, d1)] -= 2 * xyi / pow(2, k)
+        d1 = pref_prec[i] + k
+        Q[(d1, d1)] -= 2 * xyi / pow(2, k - 2)
 
 
 sampler = EmbeddingComposite(DWaveSampler())
@@ -96,10 +110,10 @@ for sample, energy in sampleset.data(['sample', 'energy']):
 sol_no = 1
 for di in distributions:
     wts = np.array([0.0 for i in range(d + 1)])
-    for x in range(dim):
-        i = x // precision
-        k = x % precision # The p^th of the bits we are using to represent the i^th item
-        wts[i] += di[x] / pow(2, k)
+    for j in range(d + 1):
+        precision = pref_prec[j + 1] - pref_prec[j]
+        for k in range(precision):
+            wts[i] += di[pref_prec[j] + k] / pow(2, k - 2)
     if sol_no == 1:
         # print(str(sol_no) + "-")
         Y_pred = np.matmul(X, wts)
